e08cfc07845e7b2a4ce782533d57080f500801d1
Roman Pauk <pauk83258325@gmail.com>
2017-06-18 13:09:12 +0300

    add one more html generator

--- generate html.js
+++ generate html.js
@@ -33,7 +33,9 @@ const iter = (data) => {
 		}
 
 		const processedBody = body instanceof Array ? iter(body) : body;
-		return {type: 'tag', name: data[0], body: processedBody, options};
+		const result = {type: 'tag', name: data[0], body: processedBody, options};
+		console.log(result);
+		return result;
 	};
 
 	const build = (data) => iterAst(iter(data));
@@ -52,15 +54,44 @@ const iter = (data) => {
 	];
 
 	const dat = ['html', [
-      ['head', [
-        ['title', 'hello, hexlet!'],
-      ]],
-      ['body', { class: 'container' }, [
-        ['h1', { class: 'header' }, 'html builder example'],
-        ['div', [
-          ['span'],
-          ['span', { class: 'text', id: 'uniq-key' }],
-        ]],
-      ]],
-    ]];
+			['head', [
+				['title', 'hello, hexlet!'],
+			]],
+			['body', { class: 'container' }, [
+				['h1', { class: 'header' }, 'html builder example'],
+				['div', [
+					['span'],
+					['span', { class: 'text', id: 'uniq-key' }],
+				]],
+			]],
+		]];
 	console.log(build(dat));
+
+
+
+// Other variant
+
+const argTypes = {
+	body: a => typeof a === 'string',
+	children: a => a instanceof Array,
+	attrs: a => a instanceof Object,
+};
+
+const getArgName = arg =>
+Object.keys(argTypes).filter(k => argTypes[k](arg))[0];
+
+const buildAttrString = attrs =>
+Object.keys(attrs).map(key => ` ${key}="${attrs[key]}"`).join('');
+
+const buildHtml = tagArr => {
+	const tag = tagArr.slice(1)
+	.reduce((acc, arg) => ({ ...acc, [getArgName(arg)]: arg }),
+		{ name: tagArr[0], attrs: {}, body: '', children: [] });
+
+	return [`<${tag.name}${buildAttrString(tag.attrs)}>`,
+	`${tag.body}${tag.children.map(buildHtml).join('')}`,
+	`</${tag.name}>`,
+	].join('');
+};
+
+//export default buildHtml;
